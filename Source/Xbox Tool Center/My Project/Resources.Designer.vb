dc89ffc519bba1efa235f8b92f3e0e12ba5332fb
EmuZONE <emuzone@outlook.com>
2018-01-01 04:18:01 +0100

    Add files via upload

--- Source/Xbox Tool Center/My Project/Resources.Designer.vb
+++ Source/Xbox Tool Center/My Project/Resources.Designer.vb
@@ -63,6 +63,16 @@ Namespace My.Resources
         '''<summary>
         '''  Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property Button_Green() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Button_Green", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property cooltext270620329015549() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("cooltext270620329015549", resourceCulture)
